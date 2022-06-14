89020113a2dea3ca9aef5eaff1c4899718d3c86a
Артемчук Алексей <elusiv@mail.ru>
2022-06-15 00:15:43 +0500

    fix eslint 14.06.22

--- src/js/__tests__/for...inTest.js
+++ src/js/__tests__/for...inTest.js
@@ -1,13 +1,15 @@
-import { sortingFunc } from '../for...in';
+import sortingFunc from '../for...in';
 
 test('test sorting object', () => {
-    const sorted = [
-        {key: "name", value: "мечник"},
-        {key: "level", value: 2},
-        {key: "attack", value: 80},
-        {key: "defence", value: 40},
-        {key: "health", value: 10}
-    ]
-    const result = sortingFunc({name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}, ["name", "level"])
-    expect(result).toEqual(sorted)
-})
+  const sorted = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
+  const result = sortingFunc({
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  }, ['name', 'level']);
+  expect(result).toEqual(sorted);
+});
